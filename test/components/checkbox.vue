0553e69f0835b950cc13569dbcb06278c670e18e
Eduardo San Martin Morote <i@posva.net>
2015-10-11 23:09:23 +0200

    Use jade syntax for id

--- test/components/checkbox.vue
+++ test/components/checkbox.vue
@@ -2,9 +2,9 @@
 div
   mdl-checkbox(id='check-dyn' v-bind:checked.sync='check') Dynamic {{check}}
   mdl-checkbox(id='check' v-bind:checked.sync='check' v-bind:disabled='disabled') Check me
-  input(type='checkbox' id='classic' v-model='check')
+  input#classic(type='checkbox' v-model='check')
   span Classic checkbox
-  input(type='checkbox' id='disable' v-model='disabled')
+  input#disable(type='checkbox' v-model='disabled')
   span Disable
   mdl-checkbox(v-if='disabled' id='v-if' v-bind:checked.sync='check' v-bind:disabled='disabled') v-if
   mdl-checkbox(v-for='n in 3' v-bind:value='indexId(n)' v-bind:id='indexId(n)' v-bind:checked.sync='checks') v-for {{indexId(n)}}
