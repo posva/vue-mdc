9dd39830f29cf30b72d3440b831e0cce7727ec3d
Eduardo San Martin Morote <i@posva.net>
2015-12-01 14:32:51 +0100

    Automatic doc deploy

--- build/gh-pages.sh
+++ build/gh-pages.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 br=$(git rev-parse --abbrev-ref HEAD)
-git pull --rebase && \
 git checkout gh-pages && \
+git pull --rebase || exit 1
+
+if ! git config user.email || ! git config user.name; then
+  git config user.email "i@posva.net"
+  git config user.name "Circle CI"
+fi
 git pull --rebase && \
-git merge doc && \
+git merge --no-edit $br && \
 npm run doc && \
-git commit -a --amend -m "$(date)" && \
+git commit -a --amend --no-edit && \
 git push origin gh-pages && \
 git checkout $br || exit 1
