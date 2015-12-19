f23857ff24b85cc4d8d5e4ec92ad3d55fb34c41c
Eduardo San Martin Morote <i@posva.net>
2015-12-19 13:13:47 +0100

    Removed bad indentation in textfield jade

--- src/textfield.vue
+++ src/textfield.vue
@@ -1,17 +1,17 @@
 <template lang="jade">
-  .mdl-textfield.mdl-js-textfield(v-bind:class='{"mdl-textfield--floating-label": floatingLabel, "mdl-textfield--expandable": expandable}')
-    slot(v-if='expandable' name='expandable-button')
-      label.mdl-button.mdl-js-button.mdl-button--icon(v-bind:for.once='id')
-        i.material-icons {{expandable}}
-    div(v-bind:class='{"mdl-textfield__expandable-holder": expandable}')
-      slot(v-if='textarea' name='textarea')
-        textarea.mdl-textfield__input(type='text' v-bind:id.once='id' v-bind:rows='rows')
-      slot(v-else name='input')
-        input.mdl-textfield__input(v-bind:type='type' v-model='value' v-bind:id.once='id' v-bind:pattern='pattern')
-      slot(name='label')
-        label.mdl-textfield__label(v-bind:for.once='id') {{label}}
-      slot(name='error')
-        label.mdl-textfield__error(v-if='error') {{error}}
+.mdl-textfield.mdl-js-textfield(v-bind:class='{"mdl-textfield--floating-label": floatingLabel, "mdl-textfield--expandable": expandable}')
+  slot(v-if='expandable' name='expandable-button')
+    label.mdl-button.mdl-js-button.mdl-button--icon(v-bind:for.once='id')
+      i.material-icons {{expandable}}
+  div(v-bind:class='{"mdl-textfield__expandable-holder": expandable}')
+    slot(v-if='textarea' name='textarea')
+      textarea.mdl-textfield__input(type='text' v-bind:id.once='id' v-bind:rows='rows')
+    slot(v-else name='input')
+      input.mdl-textfield__input(v-bind:type='type' v-model='value' v-bind:id.once='id' v-bind:pattern='pattern')
+    slot(name='label')
+      label.mdl-textfield__label(v-bind:for.once='id') {{label}}
+    slot(name='error')
+      label.mdl-textfield__error(v-if='error') {{error}}
 </template>
 
 <script lang="coffee">
