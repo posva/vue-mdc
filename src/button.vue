34767096ea6e1036dd2a8f627637c34d057d186d
Eduardo San Martin Morote <i@posva.net>
2016-01-16 20:29:38 +0100

    Fixed wrong indentation in button

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template lang="jade">
-  button.mdl-button.mdl-js-button(v-bind:disabled='disabled' v-bind:class='{"mdl-button--icon": icon, "mdl-button--accent": accent, "mdl-button--primary": primary, "mdl-button--mini-fab": miniFab, "mdl-button--fab": fab || miniFab, "mdl-button--raised": raised, "mdl-button--colored": colored}')
-    slot
+button.mdl-button.mdl-js-button(v-bind:disabled='disabled' v-bind:class='{"mdl-button--icon": icon, "mdl-button--accent": accent, "mdl-button--primary": primary, "mdl-button--mini-fab": miniFab, "mdl-button--fab": fab || miniFab, "mdl-button--raised": raised, "mdl-button--colored": colored}')
+  slot
 </template>
 
 <script lang="coffee">
