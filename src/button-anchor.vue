5753424acfe95cee59a31fcb9f8715d49d6d8f75
Eduardo San Martin Morote <i@posva.net>
2016-02-06 23:58:42 +0100

    Moved button component to a mixin and added mdl-button as an anchor

--- src/button-anchor.vue
+++ src/button-anchor.vue
@@ -1,5 +1,5 @@
 <template lang="jade">
-button.mdl-button.mdl-js-button(v-bind:disabled='disabled' v-bind:class='cssClasses')
+a.mdl-button.mdl-js-button(v-bind:href='href' v-bind:disabled='disabled' v-bind:class='cssClasses')
   slot
     i.material-icon(v-if='icon') {{icon}}
 </template>
@@ -8,5 +8,9 @@ button.mdl-button.mdl-js-button(v-bind:disabled='disabled' v-bind:class='cssClas
 button = require './mixins/button.coffee'
 
 module.exports =
+  props:
+    href:
+      type: String
+      required: true
   mixins: [button]
 </script>
