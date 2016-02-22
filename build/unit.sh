db34d72eaf6130a4854cc1c6932b812663ac471c
Eduardo San Martin Morote <i@posva.net>
2016-02-22 01:11:39 +0100

    Fix CI

--- build/unit.sh
+++ build/unit.sh
@@ -5,6 +5,10 @@ export PORT=8082
 # serve example
 node build/dev-server.js &
 
+if [[ "$CI" ]]; then
+  sleep 2
+fi
+
 # run e2e tests, make sure to kill the server no matter pass or fail
 ./node_modules/.bin/mocha-phantomjs "http://localhost:$PORT/test/unit" \
   && kill $! || (kill $! && exit 1)
