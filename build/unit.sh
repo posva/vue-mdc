d6e3345809fdd02359d328c7eb21b4c42e473606
Eduardo San Martin Morote <i@posva.net>
2016-02-23 17:30:33 +0100

    Fix missing dep vue

--- build/unit.sh
+++ build/unit.sh
@@ -5,8 +5,9 @@ export PORT=8082
 # serve example
 node build/dev-server.js &
 
+# CI can be slower and this prevents some random timeout errors
 if [[ "$CI" ]]; then
-  sleep 2
+  sleep 4
 fi
 
 # run e2e tests, make sure to kill the server no matter pass or fail
