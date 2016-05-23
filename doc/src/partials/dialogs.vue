6ec1f51594421fc2f4abbf194744f18b990a86df
Eduardo San Martin Morote <posva13@gmail.com>
2016-05-24 00:04:12 +0200

    Improve dialogs to make them more customisable
    Closes #34

--- doc/src/partials/dialogs.vue
+++ doc/src/partials/dialogs.vue
@@ -7,21 +7,21 @@
   .section__content
     p The dialog component allows for verification of user actions, simple data input, and alerts to provide extra information to users. To display dialogs, use events.
 
-    div(v-transfer-dom) Hello
-      mdl-dialog(title='Hi there' display-on='infoMessage' cancellable)
+    div(v-transfer-dom)
+      mdl-dialog(v-ref:info-message title='Hi there')
         p Hello. This is an information message. You can click outside or in the close button to close it.
     .flex.center.wrap
-      mdl-button(fab, primary, v-on:click='$broadcast("infoMessage")')
+      mdl-button(fab, primary, v-on:click='$refs.infoMessage.open')
         i.material-icons power_settings_new
     pre
       code.html
-        p= '<mdl-dialog title="Hi there" display-on="infoMessage" cancellable>'
+        p= '<mdl-dialog v-ref:info-message title="Hi there">'
         p= '  <p>Hello. This is an information message. You can click outside or in the close button to close it.</p>'
-        p='</mdl-dialog>'
+        p= '</mdl-dialog>'
 
       code.javascript.
         // Inside the parent vm
-        this.$broadcast('infoMessage')
+        this.$refs.infoMessage.open()
 
     p Sometimes the modal need to be appended to the dom. In order to do that you
       | you'll need the 
@@ -33,52 +33,59 @@
     pre
       code.html
         p= '<div v-transfer-dom>'
-        p= '  <mdl-dialog title="Hi there" display-on="infoMessage" cancellable>'
+        p= '  <mdl-dialog v-ref:info-message title="Hi there">'
         p= '    <p>Hello. This is an information message. You can click outside or in the close button to close it.</p>'
-        p='  </mdl-dialog>'
-        p='</div>'
+        p= '  </mdl-dialog>'
+        p= '</div>'
 
     p The example above use this technique to prevent the menu from overlapping.
 
-    p By default there's only one action. You can add more actions by using the slot
-      code  actions
-      | . Each action must either be a
-      code  button
-      |  or contain an attribute
-      code  data-action
-      | .
+    p The dialgo component emits an&nbsp;
+      code open
+      |  event when it opens as well as a&nbsp;
+      code close
+      |  event when it closes by clicking the Close button. You can use those events
+      | to fire actions when the dialog is closed.
 
     pre
       code.html
-        p= '<mdl-dialog title="Hi there" display-on="infoMessage">'
+        p= '<mdl-dialog @close="closeCallback" v-ref:info-message title="Hi there">'
+        p= '  <p>Hello. This is an information message. You can click outside or in the close button to close it.</p>'
+        p= '</mdl-dialog>'
+
+      code.javascript.
+        methods: {
+          closeCallback () {
+            // do something
+          }
+        }
+
+    p If you want to customise actions, use the&nbsp;
+      code actions
+      |  slot:
+
+    div(v-transfer-dom)
+      mdl-dialog(v-ref:multiple full-width title='Hi there')
+        p Hello.
+        p Number is {{ number }}
+        p Increase the number or decrease without closing this modal
+        template(slot="actions")
+          mdl-button(primary, v-on:click='number++') Increase
+          mdl-button(primary, v-on:click='number--') Decrease
+          mdl-button(v-on:click='$refs.multiple.close') Close
+    .flex.center.wrap
+      mdl-button(fab, primary, v-on:click='$refs.multiple.open')
+        i.material-icons power_settings_new
+    pre
+      code.html
+        p= '<mdl-dialog v-ref:multiple full-width title="Hi there">'
         p= '  <p>Hello</p>'
         p= '  <template slot="actions">'
-        p= '    <mdl-button primary>Increase</mdl-button>'
-        p= '    <mdl-button primary>Decrease</mdl-button>'
-        p= '    <mdl-button>Close</mdl-button>'
+        p= '    <mdl-button primary @click="number++">Increase</mdl-button>'
+        p= '    <mdl-button primary @click="number--">Decrease</mdl-button>'
+        p= '    <mdl-button @click="$refs.multiple.close">Close</mdl-button>'
         p= '  </template>'
-        p='</mdl-dialog>'
-
-    p You can pass any number of callbacks as actions in the same order as the buttons when emitting the event.
-
-    pre
-      code.javascript.
-        // Inside the parent vm
-        this.$broadcast('infoMessage', increaseFunction, decreaseFunction, closeCallback)
-
-    p If you added the 
-      code cancellable
-      |  prop to the dialog, you can specify one extra callback. If there's no extra callback, the callback used for the last action is used. This callback is only called when the dialog is dismissed by clicking outside of it.
-
-    pre
-      code.javascript.
-        // Inside the parent vm
-        this.$broadcast('infoMessage', actionFunc, closeCallback, dismissCallback)
-
-    p By default all actions closes the dialog, if you want to prevent that you must explicitly return false in the callback.
-    p You can pass 
-      code null
-      |  instead of a function for any given action.
+        p= '</mdl-dialog>'
 
     h5 Prop List
     table.mdl-data-table.mdl-js-data-table
@@ -95,25 +102,21 @@
           td.mdl-data-table__cell--non-numeric Optional
         tr
           td.mdl-data-table__cell--non-numeric
-            code  display-on
-          td.mdl-data-table__cell--non-numeric Event to listen for displaying
-          td.mdl-data-table__cell--non-numeric Required
-        tr
-          td.mdl-data-table__cell--non-numeric
             code  full-width
           td.mdl-data-table__cell--non-numeric Make buttons full width by applying the 
             code  mdl-dialog__actions--full-width
             |  modifier
-          td.mdl-data-table__cell--non-numeric This prop doesn't need a value
-        tr
-          td.mdl-data-table__cell--non-numeric
-            code  cancellable
-          td.mdl-data-table__cell--non-numeric Make the dialog dismissible by clicking outside of it
           td.mdl-data-table__cell--non-numeric This prop doesn't need a value
 
     br
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      number: 0
+    }
+  }
+}
 </script>
--- test/unit/specs/Dialog.spec.js
+++ test/unit/specs/Dialog.spec.js
@@ -14,15 +14,21 @@ describe('Dialog', () => {
     info.should.not.be.visible
   })
 
-  it('shows with an event', (done) => {
-    vm.$broadcast('infoMessage')
+  it('opens', (done) => {
+    const dialog = vm.$refs.info
+    dialog.$el.style.should.have.property('display', 'none')
+    // for some reason stubs doesn't work...
+    vm.events.should.have.length(0)
+    vm.$refs.info.open()
     vm.nextTick()
     .then(() => {
-      info.should.be.visible
+      vm.events.should.have.length(1)
+      dialog.$el.style.should.not.have.property('display', 'none')
       info.querySelector('button').click()
       return vm.nextTick()
     }).then(() => {
-      info.should.not.be.visible
+      dialog.$el.style.should.have.property('display', 'none')
+      vm.events.should.have.length(2)
     }).then(done, done)
   })
 
@@ -32,90 +38,5 @@ describe('Dialog', () => {
 
   it('has a description', () => {
     info.querySelector('.mdl-dialog__content').should.have.text('This is a modal example')
-  })
-
-  it('call a callback', (done) => {
-    const modal = vm.$('#callback')
-    vm.number.should.be.eql(0)
-    vm.$broadcast('messageWithCallback', () => vm.number++)
-    vm.nextTick()
-    .then(() => {
-      vm.number.should.be.eql(0)
-      modal.querySelector('button').click()
-      return vm.nextTick()
-    }).then(() => {
-      vm.number.should.be.eql(1)
-    }).then(done, done)
-  })
-
-  it('calls a callback without closing', (done) => {
-    const modal = vm.$('#multiple')
-    vm.number = 0
-    vm.$broadcast('multipleActionsMessage', () => {
-      vm.number++
-      return false
-    }, () => vm.number--)
-    modal.should.not.be.visible
-    vm.nextTick()
-    .then(() => {
-      vm.number.should.be.eql(0)
-      modal.should.be.visible
-      modal.querySelector('button:nth-child(1)').click()
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.be.visible
-      vm.number.should.be.eql(1)
-      modal.querySelector('button:nth-child(1)').click()
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.be.visible
-      vm.number.should.be.eql(2)
-      modal.querySelector('button:nth-child(2)').click()
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.not.be.visible
-      vm.number.should.be.eql(1)
-      return vm.nextTick()
-    }).then(done, done)
-  })
-
-  it('calls a different function when cancelling', (done) => {
-    let modal = vm.$('#cancellable')
-    vm.number = 0
-    vm.$broadcast('cancellableMessage', () => {
-      vm.number = 10
-    }, () => {
-      vm.number = 5
-    })
-
-    let modalVm = vm.$children[5]
-
-    vm.nextTick()
-    .then(() => {
-      modal.should.be.visible
-      vm.number.should.be.eql(0)
-      modalVm.cancel()
-      // This doesn't work on phantomjs
-      // modal.click()
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.not.be.visible
-      vm.number.should.be.eql(5)
-      vm.$broadcast('cancellableMessage', () => {
-        vm.number = 10
-      }, () => {
-        vm.number = 5
-      })
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.be.visible
-      vm.number.should.be.eql(5)
-      modal.querySelector('button').click()
-      return vm.nextTick()
-    }).then(() => {
-      modal.should.not.be.visible
-      vm.number.should.be.eql(10)
-      return vm.nextTick()
-    }).then(done, done)
   })
 })
