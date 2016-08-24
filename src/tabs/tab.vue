00e67c4f19e3b377acba19e0a282aa3068bb25b3
Vitaly Nikolenko <vit@webstandart.ru>
2016-08-24 16:45:08 +0300

    -- Add support for  v-link

--- src/tabs/tab.vue
+++ src/tabs/tab.vue
@@ -7,42 +7,50 @@
 </template>
 
 <script>
-function tabToOject (tab) {
-  return typeof tab === 'string'
-       ? { id: tab, title: tab }
-       : { id: tab.id || tab.title, ...tab }
-}
-
-export default {
-  computed: {
-    selected () {
-      return this.$parent.isSelected(this.tabData)
-    },
-    id () {
-      return typeof this.tab === 'string'
-           ? this.tab
-           : this.tab.id || this.tab.title
-    },
-    tabData () {
-      return tabToOject(this.tab)
-    }
-  },
-  props: {
-    tab: {
-      required: true,
-      type: [String, Object]
-    }
-  },
-  watch: {
-    tab (newTab, oldTab) {
-      this.$parent.updateTab(tabToOject(oldTab), this.tabData)
-    }
-  },
-  ready () {
-    this.$parent.addTab(this.tabData)
-  },
-  beforeDestroy () {
-    this.$parent.removeTab(this.tabData)
+  function tabToObject (tab) {
+    return typeof tab === 'string'
+            ? {id: tab, title: tab}
+            : {id: tab.id || tab.title, ...tab}
   }
-}
+
+  export default {
+    computed: {
+      selected () {
+        return this.$parent.isSelected(this.tabData)
+      },
+      id () {
+        return typeof this.tab === 'string'
+                ? this.tab
+                : this.tab.id || this.tab.title
+      },
+      tabData () {
+        let tabData = tabToObject(this.tab)
+        if (this.link) {
+          tabData.link = this.link
+        }
+        return tabData
+      }
+    },
+    props: {
+      tab: {
+        required: true,
+        type: [String, Object]
+      },
+      link: {
+        required: false,
+        type: [String, Object]
+      }
+    },
+    watch: {
+      tab (newTab, oldTab) {
+        this.$parent.updateTab(tabToObject(oldTab), this.tabData)
+      }
+    },
+    ready () {
+      this.$parent.addTab(this.tabData)
+    },
+    beforeDestroy () {
+      this.$parent.removeTab(this.tabData)
+    }
+  }
 </script>
