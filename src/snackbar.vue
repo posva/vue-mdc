bce9b5116abb9cc2f663fd4ddf7669f1ce85b6c3
Peter Johason <peter@peterjohanson.com>
2016-08-26 22:43:37 -0400

    Restore event driven snackbar display.
    * Back to using eventing to displaying snackbar.
    * Because Vue 2.0 doesn't propogate/bubble events, instead allow explicitly
      setting the event-source via a prop, and fallback a default to vm.$root as the
      event source to listen to for display events.

--- src/snackbar.vue
+++ src/snackbar.vue
@@ -8,13 +8,24 @@
 /* global componentHandler*/
 
 export default {
+  props: {
+    displayOn: {
+      type: String,
+      required: true
+    },
+    eventSource: {
+      type: Object,
+      required: false,
+      default: function () {
+        return this.$root
+      }
+    }
+  },
   mounted () {
     componentHandler.upgradeElement(this.$el, 'MaterialSnackbar')
-  },
-  methods: {
-    showSnackbar: function (config) {
-      this.$el.MaterialSnackbar.showSnackbar(config)
-    }
+    this.eventSource.$on(this.displayOn, (snackbarConfig) => {
+      this.$el.MaterialSnackbar.showSnackbar(snackbarConfig)
+    })
   }
 }
 </script>
