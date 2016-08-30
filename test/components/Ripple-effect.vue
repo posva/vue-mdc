28fa1580254fb9cae515a86441f81de57c98b085
Peter Johason <peter@peterjohanson.com>
2016-08-30 10:06:48 -0400

    Fix up usage of toggle/radios in the ripple effect test.

--- test/components/Ripple-effect.vue
+++ test/components/Ripple-effect.vue
@@ -3,10 +3,10 @@ div
   h1 Ripple Effect
   h3 vue
   mdl-button#button-ripple(mdl-ripple-effect) Hello
-  mdl-checkbox#checkbox-ripple(mdl-ripple-effect type="checkbox" v-model='check') Checkbox
-  mdl-radio#radio-ripple(mdl-ripple-effect value='one' type="radio" v-model='check') Radio
-  mdl-switch#switch-ripple(mdl-ripple-effect type="checkbox" v-model='check')
-  mdl-icon-toggle#icon-toggle-ripple(mdl-ripple-effect type="checkbox" v-model='check' icon='add')
+  mdl-checkbox#checkbox-ripple(mdl-ripple-effect v-model='check') Checkbox
+  mdl-radio#radio-ripple(mdl-ripple-effect selected-value='one' v-model='check') Radio
+  mdl-switch#switch-ripple(mdl-ripple-effect v-model='check')
+  mdl-icon-toggle#icon-toggle-ripple(mdl-ripple-effect v-model='check' icon='add')
 </template>
 
 <script lang="babel">
