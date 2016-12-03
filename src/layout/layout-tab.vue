a4a951119047b9c6473eb1f772253d2a0dc17c58
Ben Force <benforce@gmail.com>
2016-12-02 23:53:32 -0600

    Adding layout elements

---
+++ src/layout/layout-tab.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="mdl-layout__tab-panel"
+            :class="activeClass"
+            :id="id">
+      <div class="page-content">
+        <slot></slot>  
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    this.$root.$emit('layout-tab-created', this)
+  },
+  computed: {
+    referenceLink () {
+      return '#' + this.id
+    },
+    activeClass () {
+      return {
+        'is-active': this.isActive
+      }
+    }
+  },
+  data: function () {
+    return {
+      id: '',
+      isActive: false
+    }
+  },
+  props: {
+    title: String
+  }
+}
+</script>
