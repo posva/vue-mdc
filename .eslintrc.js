ea247ee8e55ed69c722b3763cd8bd6f280216d50
Eduardo San Martin Morote <posva13@gmail.com>
2017-02-17 20:09:32 +0100

    ✅ Add lint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  extends: 'vue',
+  // add your custom rules here
+  'rules': {
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
