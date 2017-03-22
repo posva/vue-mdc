b89ebd4dbff9aa6b41a66c3ecbe6cae6ca92525c
Eduardo San Martin Morote <posva13@gmail.com>
2017-03-22 20:10:05 +0100

    Update deploy script

--- build/gh-pages.sh
+++ build/gh-pages.sh
@@ -1,18 +1,11 @@
 #! /bin/bash
-br=$(git rev-parse --abbrev-ref HEAD)
-git checkout gh-pages && \
-git pull --rebase || exit 1
-
 if ! git config user.email || ! git config user.name; then
   git config user.email "i@posva.net"
   git config user.name "Circle CI"
 fi
 
-git pull --rebase && \
-git merge -X theirs --no-edit $br && \
 npm run doc && \
 cp dist/index.html dist/doc-bundle.js doc/favicon.ico . && \
 git add -f index.html doc-bundle.js favicon.ico && \
 git commit -a --amend --no-edit && \
 git push origin gh-pages && \
-git checkout $br || exit 1
