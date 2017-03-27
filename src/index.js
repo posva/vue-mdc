138be12e2fed615fbc803941e769cf2ed06ae601
Eduardo San Martin Morote <posva13@gmail.com>
2017-03-28 00:36:31 +0200

    ✨ Export card subcomponents too
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
-import MdcCard from './Card/Card.vue'
-import MdcButton from './Button.vue'
-import ripple from './ripple.js'
+import Card from './Card/Card'
+import CardPrimary from './Card/CardPrimary'
+import CardTitle from './Card/CardTitle'
+import CardSubtitle from './Card/CardSubtitle'
+import CardActions from './Card/CardActions'
+import CardMedia from './Card/CardMedia'
+import CardSupportingText from './Card/CardSupportingText'
+import Button from './Button'
+import ripple from './ripple'
 
 // import './style.scss'
 
-function plugin (Vue) {
-  // Vue.component('mdc-ripple', MdcRipple)
-  Vue.component('MdcCard', MdcCard)
-  Vue.component('MdcButton', MdcButton)
+const components = {
+  Card,
+  CardPrimary,
+  CardTitle,
+  CardSubtitle,
+  CardActions,
+  CardMedia,
+  CardSupportingText,
+  Button,
+}
+
+function plugin (Vue, opts = { prefix: 'Mdc' }) {
+  const { prefix } = opts
+  const compNames = Object.keys(components)
+  for (let i = 0; i < compNames.length; i++) {
+    const name = compNames[i]
+    Vue.component(`${prefix}${name}`, components[name])
+  }
+  Vue.directive('ripple', ripple)
 }
 
 export default plugin
 // Export all components too
 const version = '__VERSION__'
 export {
-  MdcButton,
-  MdcCard,
   version,
+
+  // components
+  Card,
+  CardPrimary,
+  CardTitle,
+  CardSubtitle,
+  CardActions,
+  CardMedia,
+  CardSupportingText,
+  Button,
+
+  // directives
   ripple,
 }
