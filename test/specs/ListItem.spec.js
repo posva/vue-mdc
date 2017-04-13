c95c0f3d5396da8d41ddb8a17b23bf5bcbb5d1c6
Eduardo San Martin Morote <posva13@gmail.com>
2017-04-13 17:11:10 +0200

    ✅ Add list item tests
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

--- test/specs/ListItem.spec.js
+++ test/specs/ListItem.spec.js
@@ -6,12 +6,19 @@ import {
 
 describe('ListItem', function () {
   it('renders an upgraded list item', function () {
-    const vm = createVM(this, `
-<ListItem>Item 1</ListItem>
-`, {
-  components: { ListItem },
-})
-    vm
+    const vm = createVM(this, h => (
+      <ListItem ref='item'>Item 1</ListItem>
+    ))
+    vm.$refs.item.should.have.class('mdc-list-item')
+    vm.$refs.item.should.match('li')
+  })
+
+  it('can render a custom tag', function () {
+    const vm = createVM(this, h => (
+      <ListItem ref='item' tag='a'>Hello</ListItem>
+    ))
+    vm.$refs.item.should.have.class('mdc-list-item')
+    vm.$refs.item.should.match('a')
   })
 
   it('keeps original tag data', dataPropagationTest(ListItem))
