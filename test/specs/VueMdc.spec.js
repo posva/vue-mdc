4d57f78aae3f44e8f2449b9ff99d27e190c13239
Eduardo San Martin Morote <posva13@gmail.com>
2017-04-30 13:17:35 +0200

    ✅ Refactor component testing
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

--- test/specs/VueMdc.spec.js
+++ test/specs/VueMdc.spec.js
@@ -13,33 +13,39 @@ function dir (name) {
   }
 }
 
+const components = [
+  'Button',
+
+  'Checkbox',
+
+  'Card',
+  'Card',
+  'CardActions',
+  'CardHorizontalBlock',
+  'CardMedia',
+  'CardPrimary',
+  'CardSubtitle',
+  'CardSupportingText',
+  'CardTitle',
+
+  'Drawer',
+  'DrawerHeader',
+  'DrawerNav',
+  'DrawerNavItem',
+
+  'List',
+  'ListDivider',
+]
+
 describe('VueMdc', function () {
   describe('Register components', function () {
     before(function () {
       Vue.use(VueMdc)
     })
 
-    it('registers MdcButton', comp('MdcButton'))
-
-    it('registers MdcCheckbox', comp('MdcCheckbox'))
-
-    it('registers MdcCard', comp('MdcCard'))
-    it('registers MdcCard', comp('MdcCard'))
-    it('registers MdcCardActions', comp('MdcCardActions'))
-    it('registers MdcCardHorizontalBlock', comp('MdcCardHorizontalBlock'))
-    it('registers MdcCardMedia', comp('MdcCardMedia'))
-    it('registers MdcCardPrimary', comp('MdcCardPrimary'))
-    it('registers MdcCardSubtitle', comp('MdcCardSubtitle'))
-    it('registers MdcCardSupportingText', comp('MdcCardSupportingText'))
-    it('registers MdcCardTitle', comp('MdcCardTitle'))
-
-    it('registers MdcDrawer', comp('MdcDrawer'))
-    it('registers MdcDrawerHeader', comp('MdcDrawerHeader'))
-    it('registers MdcDrawerNav', comp('MdcDrawerNav'))
-    it('registers MdcDrawerNavItem', comp('MdcDrawerNavItem'))
-
-    it('registers MdcList', comp('MdcList'))
-    it('registers MdcListDivider', comp('MdcListDivider'))
+    components.forEach(name => {
+      it(`registers Mdc${name}`, comp(`Mdc${name}`))
+    })
 
     it('registers v-ripple', dir('ripple'))
   })
