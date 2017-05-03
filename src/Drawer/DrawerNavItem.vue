1764871b61906758ac2a79169cdb2ef462b54e87
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-03 14:24:18 +0200

    🔨 Differentiate props and data in comp
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

--- src/Drawer/DrawerNavItem.vue
+++ src/Drawer/DrawerNavItem.vue
@@ -13,17 +13,19 @@ export default {
   inject: ['mode'],
 
   // transform into functional once inject works
+  // Actually it doesn't work yet because since it's used in
+  // a slot, therefore its parent it's the container, not the Drawer
   render (h) {
     const data = {
       'class': {
         [`mdc-${this.mode}-drawer--selected`]: this.selected,
       },
-      props: {
-        tag: this.tag,
-      },
     }
     return h(ListItem, {
       ...data,
+      props: {
+        tag: this.tag,
+      },
     }, this.$slots.default)
   },
 
