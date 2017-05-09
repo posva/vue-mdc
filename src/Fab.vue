1d3e9946da7880d52a8e9a41ac693d4b0dcb276e
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-05-09 09:46:25 +0000

    Add fab (#192)

---
+++ src/Fab.vue
@@ -0,0 +1,25 @@
+<script>
+import { propsToClasses } from './utils'
+
+export default {
+  functional: true,
+
+  props: {
+    mini: Boolean,
+    plain: Boolean,
+  },
+
+  render (h, { data, props, children }) {
+    const staticClass = propsToClasses('mdc-fab', props)
+    data.staticClass = data.staticClass
+                     ? `${data.staticClass} ${staticClass}`
+                     : staticClass
+    return h('button', {
+      ...data,
+    }, children)
+  },
+}
+</script>
+
+<style lang="scss" src="@material/fab/mdc-fab.scss">
+</style>
